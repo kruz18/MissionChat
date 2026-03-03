e487d166aa6356ee77535cca05039019b6b940db
KYamshanov <kyamshanov@vk.com>
2026-03-03 11:14:09 +0400

    fix

--- app/byCompose/common/src/commonMain/kotlin/ru/kyamshanov/missionChat/welcomeScreen/MessagesList.kt
+++ app/byCompose/common/src/commonMain/kotlin/ru/kyamshanov/missionChat/welcomeScreen/MessagesList.kt
@@ -6,17 +6,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -31,12 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -96,7 +81,7 @@ fun MessagesList(
             }
 
             ChatCard(
-                modifier = Modifier.animateItem(),
+                modifier = Modifier.animateItem(placementSpec = null),
                 icon = icon,
                 iconContentDescription = iconDescription,
                 title = it.name,
